9fd8374c85c37209134187578532e1df7e3165c0
Marcos Lima <marcoslima@Marcoss-MacBook-Pro.local>
2024-08-08 13:27:53 -0300

    tableHTML ranking of countries

--- R/brazilian_medals.R
+++ R/brazilian_medals.R
@@ -9,7 +9,7 @@ if (!require("pacman")) install.packages("pacman")
 
 # Use pacman to load add-on packages as desired
 # Packages I load every time; uses "pacman"
-pacman::p_load(pacman,tidyverse,rvest,grid,gridExtra) 
+pacman::p_load(pacman,tidyverse,rvest,grid,gridExtra,tableHTML) 
 
 
 
@@ -122,10 +122,10 @@ plot_medals_per_year <- function(df=read_table_medals_per_year_df()){
 plot_athlete_medals_per_year <- function(df=read_table_medals_per_athlete_df(),medals_per_year_df=read_table_medals_per_year_df(),max_input=NULL,text_size=5,sport_input="vela",title_on=T){
   if(is.null(max_input)) max_input=medals_year_sport_max(df)
   if(text_size<5) { df$medalha_nome <- df$medalha_nome_8 
-           } else { df$medalha_nome <- df$medalha_nome_13 }
-  plot <- df %>%
-    filter(Esporte==sport_input) %>% 
-    full_join(medals_per_year_df %>% mutate(Ano=as.character(Ano)) %>% distinct(Ano),by="Ano") %>%
+  } else { df$medalha_nome <- df$medalha_nome_13 }
+  df <- df %>% filter(Esporte==sport_input)
+  if(!is.null(medals_per_year_df)) df <- df %>% full_join(medals_per_year_df %>% mutate(Ano=as.character(Ano)) %>% distinct(Ano),by="Ano")
+  plot <- df %>% 
     group_by(year,medalha_factor,Ano,Medalha,Esporte,medalha_nome) %>% summarise(N=n(),.groups="keep") %>%
     # tail(10)
     arrange(year,desc(medalha_factor)) %>% mutate(N=ifelse(is.na(Medalha),0,N)) %>% mutate(Esporte=sport_input) %>%
@@ -150,7 +150,69 @@ plot_athlete_medals_per_year <- function(df=read_table_medals_per_athlete_df(),m
 }
 
 
+
+
+rvest_wiki_medals_country <- function(){
+  url1="https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table"
+  url2="https://pt.wikipedia.org/wiki/Quadro_de_medalhas_dos_Jogos_Ol%C3%ADmpicos"
+  return_list <- list()
+  cat(paste("Reading from",url1,"\n"))
+  return_list[[1]] <- url1 %>% read_html()
+  cat(paste("Reading from",url2,"\n"))
+  return_list[[2]] <- url2 %>% read_html()
+  return(return_list)
+}
+
+tableHTML_medals_country <- function(medals_country_html=rvest_wiki_medals_country()){
+  # load medals per year
+  df <- medals_country_html[[2]] %>% html_node(xpath='//*[@id="mw-content-text"]/div[1]/table[1]') %>% html_table() 
+  df <- df[,1:6]
+  colnames(df) <- c("team","participate","gold","silver","bronze","total")
+  df <- df %>% as_tibble() %>% tail(-1) %>% head(-1) %>% 
+    mutate(acronymn=substr(team,1,3)) %>% 
+    mutate(team=substr(team,5,nchar(team))) %>% 
+    mutate(team=gsub("\\[.*?\\]","",team)) %>%
+    mutate(team=gsub("\\(.*?\\)","",team)) %>%
+    mutate(gold=str_remove_all(gold,","),silver=str_remove_all(silver,","),bronze=str_remove_all(bronze,","),total=str_remove_all(total,",")) %>% 
+    mutate(gold=as.integer(gold),silver=as.integer(silver),bronze=as.integer(bronze),total=as.integer(total))
+  defunct_df <- medals_country_html[[1]] %>% html_node(xpath='//*[@id="mw-content-text"]/div[1]/table[4]') %>% html_table() %>% select(Team) %>% mutate(acronymn=str_extract(Team,"\\(.*?\\)"),acronymn=str_remove_all(acronymn,"\\("),acronymn=str_remove_all(acronymn,"\\)"))
+  special_df <- medals_country_html[[1]] %>% html_node(xpath='//*[@id="mw-content-text"]/div[1]/table[5]') %>% html_table() %>% select(Team) %>% mutate(acronymn=str_extract(Team,"\\(.*?\\)"),acronymn=str_remove_all(acronymn,"\\("),acronymn=str_remove_all(acronymn,"\\)"))
+  df <- df %>% mutate(defunct=(acronymn %in% defunct_df$acronymn))
+  df <- df %>% mutate(special=(acronymn %in% special_df$acronymn))
+  df <- df %>% arrange(desc(gold)) %>% mutate(rank=row_number())
+  rank_brazil=(df %>% filter(grepl("Brasil",team)) %>% tail(1))$rank
+  df <- df %>% filter(rank<10 | (rank>(rank_brazil-5) & rank<(rank_brazil+5))) %>% head(20)
+  
+  # library(tableHTML)
+  
+  
+  df <- df %>% mutate(i=row_number())
+  df %>% select(rank,team,participate,gold,silver,bronze,total) %>% 
+    tableHTML(rownames=F,border=0,headers=c("","País","jogos","","","","total"),widths=c(40,200,80,80,80,80,80),escape=F) %>% 
+    add_css_table(css=list(c('background-color','color'),c('rgba(34,34,34,1)','rgba(226,226,226, 1)'))) %>%
+    add_css_row(css=list(c('background-color'),c('rgba(21,21,21,.9)')),rows=even(1:nrow(df))) %>%
+    add_css_row(css=list('color','rgba(80,80,80, 1)'),rows=df[df$defunct | df$special,]$i+1) %>%
+    add_css_row(css=list('font-size','22px'),rows=df[df$team=="Brasil",]$i+1) %>%
+    add_css_column(columns=4,css=list('color','rgba(255,215,  0, 1)')) %>% 
+    add_css_column(columns=5,css=list('color','rgba(190,190,190, 1)')) %>% 
+    add_css_column(columns=6,css=list('color','rgba(205,104, 57, 1)')) %>% 
+    add_css_header(headers=3:7,css=list("text-align","center")) %>% 
+    add_css_column(columns=3:7,css=list('text-align','center'))
+  
+  
+  # transform medals per year variables
+  # df <- df %>% 
+  #   mutate(Ouro=as.integer(Ouro),Prata=as.integer(Prata),Bronze=as.integer(Bronze),Total=as.integer(Total)) %>% 
+  #   mutate(rank=as.integer(str_remove(`Colocação`,"º"))) %>% 
+  #   mutate(year=factor(Ano)) %>% 
+  #   ungroup()
+  # return(df)
+}
+
+
 if(F){
+  
+  
   
   source("R/brazilian_medals.R")
   
--- R/shiny_brazilian_medals.R
+++ R/shiny_brazilian_medals.R
@@ -2,7 +2,7 @@
 
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(pacman,shiny)
+pacman::p_load(pacman,shiny,tableHTML)
 
 print(getwd())
 # print(dir(file.path(getwd())))
@@ -48,12 +48,24 @@ ui <- fluidPage(
              h2("História das conquistas olímpicas do Brasil"),
              p("A participação do Brasil na história dos Jogos Olímpicos é marcada por um crescimento gradual e significativo desde sua primeira aparição em 1920. O país iniciou sua trajetória olímpica com um pequeno grupo de atletas e ao longo das décadas aumentou seu contingente e a diversidade de esportes nos quais compete. Este desenvolvimento refletiu-se não apenas na quantidade de medalhas conquistadas, mas também na excelência em várias modalidades, consolidando o Brasil como uma nação com um forte espírito olímpico.",style="text-align: justify;"),
              plotOutput("plot_medals_per_year",height="800px"),
-             div("Visualização feita com os dados da wikipedia em",a(href="https://pt.wikipedia.org/wiki/Lista_de_medalhas_brasileiras_nos_Jogos_Ol%C3%ADmpicos","https://pt.wikipedia.org/wiki/Lista_de_medalhas_brasileiras_nos_Jogos_Ol%C3%ADmpicos")),
+             div("",a(href="https://pt.wikipedia.org/wiki/Lista_de_medalhas_brasileiras_nos_Jogos_Ol%C3%ADmpicos","Visualização feita com os dados da wikipedia em")),
              br(),
              p("Entre os destaques da trajetória brasileira, o atleta Adhemar Ferreira da Silva brilhou ao conquistar o bicampeonato olímpico no salto triplo em 1952 e 1956. No campo dos esportes, a vela tem sido particularmente bem-sucedida, destacando-se com duas medalhas de ouro nos Jogos de 1980. O judô é outro esporte de grande destaque para o Brasil, sendo a modalidade que mais trouxe medalhas ao país ao longo dos anos. Por fim, o vôlei merece uma menção especial, com as seleções de vôlei de quadra conquistando quatro ouros consecutivos entre 2004 e 2016, firmando-se como uma potência mundial nesta modalidade.",style="text-align: justify;"),
              br(),
-             uiOutput("img_main_sports"),
-             br()
+             # uiOutput("img_main_sports"),
+             plotOutput("plot_vela"      ,height="400px"),br(),
+             plotOutput("plot_atletismo" ,height="600px"),br(),
+             plotOutput("plot_judo"      ,height="400px"),br(),
+             plotOutput("plot_voleibol"  ,height="400px"),br(),
+             plotOutput("plot_natacao"   ,height="400px"),br(),
+             br(),
+             p("Embora o quadro de medalhas e o ranking de países sejam populares entre os espectadores e a mídia, o Comitê Olímpico Internacional (COI) não reconhece oficialmente tais classificações. O COI enfatiza que os Jogos Olímpicos são uma celebração do esporte e da competição pacífica entre nações, e não uma disputa entre países. Dessa forma, a contagem de medalhas e o ranking são utilizados principalmente para fins informativos e de entretenimento, sem qualquer peso oficial na organização ou nos princípios dos Jogos Olímpicos.",style="text-align: justify;"),
+             br(),
+             tableHTML_output("tableHTML_medals_country"),
+             br(),
+             p("",style="text-align: justify;"),
+             br(),
+             br(),br(),br()
              ),
     tabPanel("por Esporte",
              fluidRow(
@@ -71,6 +83,14 @@ server <- function(input, output, session) {
   
   output$plot_medals_per_year <- renderPlot({ plot_medals_per_year(medals_per_year_df) })
   output$img_main_sports <- renderUI({ img(src="athletes_medals_90_degrees.png",width=input$dimension[1]-40) })
+  output$plot_vela      <- renderPlot({ plot_athlete_medals_per_year(athlete_medals_per_year_df,NULL,max_medals,4,"vela"        ) })
+  output$plot_atletismo <- renderPlot({ plot_athlete_medals_per_year(athlete_medals_per_year_df,NULL,max_medals,4,"atletismo"   ) })
+  output$plot_judo      <- renderPlot({ plot_athlete_medals_per_year(athlete_medals_per_year_df,NULL,max_medals,5,"judô"        ) })
+  output$plot_voleibol  <- renderPlot({ plot_athlete_medals_per_year(athlete_medals_per_year_df,NULL,max_medals,5,"voleibol"    ) })
+  output$plot_natacao   <- renderPlot({ plot_athlete_medals_per_year(athlete_medals_per_year_df,NULL,max_medals,5,"natação"     ) })
+  
+  output$tableHTML_medals_country <- render_tableHTML({ tableHTML_medals_country() })
+  
   
   output$plot_athlete_medals_per_year <- renderPlot({
     cat(paste("sport_select_input",input$sport_select_input,"\n"))
